4870abb63b7081a0635c35875f617e0d0683d284
bolojutsu <200Mshinobi@gmail.com>
2024-10-10 12:59:01 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const moblieMenuToggle = document.querySelector('.mobile-menu-toggle');
-const mobilelMenu = document.querySelector('.mobile-menu');
-
-moblieMenuToggle.addEventListener("click", ()=>{
-    mobilelMenu.classList.toggle('active');
-    document.body.classList.toggle('menu-open');
-});
